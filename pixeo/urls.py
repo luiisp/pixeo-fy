126e6c05f98d01c26f68ff7fa8fef77a0fd01b16
scr6w <playerzone06@gmail.com>
2024-03-29 15:22:00 -0300

    add jwt authorization with rest_framework_simplejwt

--- pixeo/urls.py
+++ pixeo/urls.py
@@ -5,7 +5,7 @@ from usrpixeo.api import viewsets as viewsets
 from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 routes = routers.DefaultRouter()
@@ -14,5 +14,7 @@ routes.register(r'images', viewsets.ImagesViewSet,basename="Images")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include(routes.urls)),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- usrpixeo/api/viewsets.py
+++ usrpixeo/api/viewsets.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from usrpixeo.api import serializers
+from rest_framework.permissions import IsAuthenticated
 from usrpixeo import models
 
 class ImagesViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = serializers.ImagesSerializer
     queryset = models.Images.objects.all()
